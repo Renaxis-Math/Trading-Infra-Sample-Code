8d888be0f5db197de9a8a3d85622b648d197ccc0
HoangChu-Claremont <chu.hoang322@gmail.com>
2023-09-13 10:04:10 -0700

    add more edge cases for build_feature_map

--- code/helper.py
+++ code/helper.py
@@ -9,9 +9,10 @@ importlib.reload(consts)
 
 def build_feature_map(filename: str, filetype: str = None) -> dict[str, str]:
     """Return a feature_name_str -> feature_description_str map,
-    given that each line in the input file 'filename' has this format:
+    given that each line in the input file 'filename' has this STRICT format:
     
-    'feature_name' - 'feature_description'
+    'feature_name' - 'feature_description' 
+    [a string (or empty), a space, '-', a space, a string (or empty)]
 
     Args:
         filename (str): the filename you want to use. Must specify file type
@@ -25,48 +26,60 @@ def build_feature_map(filename: str, filetype: str = None) -> dict[str, str]:
         print(f"Returned Empty. '{filename}' is not a valid file name")
         return {}
     
-    if filetype.count('.') > 1:
-        print(f"Returned Empty. Please remove to < 1 dot in {filetype}")
-        return {}
-    
-    if filetype.find('.') > 0:
-        print(f"Return Empty. Please only place the '.' in {filetype}'s 1st position")
-        return {}
+    if filetype:
+        if filetype.count('.') > 1:
+            print(f"Returned Empty. Please remove to < 1 dot in {filetype}")
+            return {}
+        
+        if filetype.find('.') > 0:
+            print(f"Return Empty. Please only place the '.' in {filetype}'s 1st position")
+            return {}
     # ...Edge cases
 
     # Build file path...
-    if (filetype[0] == '.'): filename = filename + filetype
-    else: filename = filename + '.' + filetype
+    if filetype:
+        if (filetype[0] == '.'): filename = filename + filetype
+        else: filename = filename + '.' + filetype
     file_path = consts.DATA_PATH + filename
     # ...Build file path
 
+    # Read the file...
+    lines = []
     try:
         file = open(file_path, "r")
         lines = file.readlines()
-        
-        # Build the feature_description_map...
-        answer = {}
-        
-        for line in lines:
-            line = line.replace("\n", '') # Remove next-line symbol
-            
-            feature, description = tuple(line.split(" - "))
-            feature = feature.strip()
-            description = description.strip()
-            
-            if feature in answer:
-                print("Feature already in FEATURE_DESCRIPTION_MAP. Updating {feature} description instead...")
-            answer[feature] = description
-        # ...Build the feature_description_map
-        
-        return answer
-    
+        file.close()
     except:
         print(f"Returned empty. Can't open file {filename}.")
         return {}
+    # ...Read the file
     
-    finally:
-        file.close()
+    # Build the feature_description_map...
+    answer = {}
+    
+    validLines_count = 0 # For testing
+    for line in lines:
+        line = line.strip()
+        if len(line) == 0: continue
+
+        validLines_count += 1
+        
+        line = line.replace("\n", '')  # Remove next-line symbol
+
+        feature, description = tuple(line.split(" - "))
+        feature = feature.strip()
+        description = description.strip()
+        
+        if len(feature) > 0:
+            if feature in answer:
+                print("Feature already in FEATURE_DESCRIPTION_MAP. Updating {feature} description instead...")
+            answer[feature] = description
+    # ...Build the feature_description_map
+    
+    assert validLines_count == len(answer), \
+    "There exists nonempty lines not included in FEATURE_DESCRIPTION_MAP"
+
+    return answer
 
 def explain_all_features() -> None:
     """Generate a well-formatted, alphabetically ordered
@@ -97,4 +110,4 @@ def explain_feature(feature_name: str = None) -> None:
     return
 
 # Local Constants
-FEATURE_DESCRIPTION_MAP = build_feature_map("data_description", "txt")
+FEATURE_DESCRIPTION_MAP = build_feature_map("data_description.txt")
