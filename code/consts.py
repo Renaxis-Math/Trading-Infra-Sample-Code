9b630aca58259e87219c22126ccfbb577d4befec
Clinic <clinic@c162.clinic.cs.hmc.edu>
2023-09-14 20:16:29 -0700

    Update Github token

--- code/consts.py
+++ code/consts.py
@@ -1,4 +1,5 @@
 # This file contains all defined constants 
 # to be used in any files within the project
 
-DATA_PATH = "./data/"
+DATA_PATH = "../data/"
+GITHUB_TOKEN = "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"
